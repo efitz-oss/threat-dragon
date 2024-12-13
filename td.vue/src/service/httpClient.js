146bbb83864254281338f6b380282bc247e61e99
syedtalha <syed.talha@devflovv.com>
2024-12-13 02:18:19 -0500

    handling errors

--- td.vue/src/service/httpClient.js
+++ td.vue/src/service/httpClient.js
@@ -51,12 +51,17 @@ const createClient = () => {
         const logAndExit = () => {
             console.log('--------logAndExit--------');
             console.error(err);
+            console.error('logAndExit---------e:---------', err.response);
             store.dispatch(LOADER_FINISHED);
             return Promise.reject(err);
         };
 
         if (err.response.status !== 401) {
             console.log('--------logAndExit-err--------');
+            console.error('Error details:-------------', err);  // Detailed error log
+        console.error('Error Response:---------1', err.response); 
+        console.error('Error Response:---------2', err.response?.message); 
+        console.error('Error Response:---------3', err.response?.body);
             return logAndExit();
         }
 
