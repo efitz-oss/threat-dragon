5a3f58b9731d7d9e6930ff3046f255e5f4065b04
Eric Fitzgerald <github@efitz.net>
2025-03-13 23:39:16 -0400

    Move migration documentation to archive directory (not tracked in git)

--- td.vue/src/plugins/bootstrap-vue.js
+++
@@ -1,3 +0,0 @@
-// This plugin is no longer used.
-// PrimeVue is now configured directly in main.js
-// This file is kept for reference only.
--- td.vue/src/plugins/toastification.js
+++
@@ -1,3 +0,0 @@
-// Toast notifications are now configured using PrimeVue ToastService
-// This functionality is set up in main.js
-// This file is kept for reference only
