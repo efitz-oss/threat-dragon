77789c24b86e1bdc55c3545ebf2ee49d7deae321
Eric Fitzgerald <github@efitz.net>
2025-04-25 14:58:43 -0400

    Footer formatting

--- td.vue/src/components/Footer.vue
+++ td.vue/src/components/Footer.vue
@@ -49,6 +49,7 @@ export default {
 .td-footer {
     background-color: colors.$orange;
     border-color: colors.$orange-alt;
+    height: sizes.$header-height - 5;
     padding: 0.75rem 0;
     margin-top: 2rem;
     border-radius: 0.3rem;
