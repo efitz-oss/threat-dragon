c0cb72a68805d480da41b1cb31377073b5136f63
Eric Fitzgerald <github@efitz.net>
2025-04-26 21:42:00 -0400

    Layout

--- td.vue/src/service/x6/shapes/store.js
+++ td.vue/src/service/x6/shapes/store.js
@@ -2,6 +2,8 @@ import { Shape } from '@antv/x6';
 
 import { tc } from '@/i18n/index.js';
 
+import defaultProperties from '@/service/entity/default-properties.js';
+
 import { ports } from '../ports.js';
 
 const name = 'store';
@@ -49,7 +51,8 @@ export const StoreShape = Shape.Path.define({
             fillOpacity: 0
         }
     },
-    ports: { ...ports }
+    ports: { ...ports },
+    data: defaultProperties.store
 });
 
 StoreShape.prototype.type = 'tm.Store';
